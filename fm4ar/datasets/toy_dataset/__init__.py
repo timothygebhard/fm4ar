5f62f53b8d4a38eafc99ab7c6fa2e7d37c4e5693
Timothy Gebhard <mail@timothygebhard.de>
2023-09-19 21:12:20 +0200

    Simplify code for toy dataset

--- fm4ar/datasets/toy_dataset/__init__.py
+++ fm4ar/datasets/toy_dataset/__init__.py
@@ -3,7 +3,6 @@ Methods for a simplistic toy dataset that can be used for debugging.
 """
 
 import warnings
-from copy import deepcopy
 
 import h5py
 import numpy as np
@@ -18,15 +17,16 @@ def simulate_toy_spectrum(
     resolution: int = 128,
 ) -> tuple[np.ndarray, np.ndarray]:
     """
-    Simple and fast function to simulate a pseudo-spectrum.
+    Simple and fast function to simulate a "pseudo-spectrum".
 
     Args:
         params: Array of parameters (arbitrary length, but >= 4).
         resolution: Resolution of the output "spectra".
 
     Returns:
-        A 2-tuple consisting of the "wavelengths" and the "flux" of
-        the pseudo-spectra.
+        A 2-tuple consisting of:
+        (1) the "wavelengths", and
+        (2) the "flux" of the pseudo-spectra.
     """
 
     # Make sure there will be some covariances in the posterior
@@ -41,12 +41,12 @@ def simulate_toy_spectrum(
     coefs = rng.normal(0, 1, (500, len(params))) @ p
 
     # Compute the pseudo-flux
-    result = 10 * np.mean(
+    flux = 10 * np.mean(
         [a_i * np.cos(i * np.pi * wlen) for i, a_i in enumerate(coefs)],
         axis=0,
     )
 
-    return wlen, result
+    return wlen, flux
 
 
 def get_posterior_samples(
@@ -123,27 +123,25 @@ def load_toy_dataset(config: dict) -> ArDataset:
     Load the toy dataset.
     """
 
-    # Do not modify the original config
-    config = deepcopy(config)
-
-    # Get the subset to load (training or test)
+    # Define shortcuts
     which = config["data"].pop("which", "train")
-    which = "train" if which == "training" else which
+    n_samples = config["data"].get("n_samples")
 
     # Load data from HDF file
     dataset_dir = get_datasets_dir() / "toy-dataset"
     file_path = dataset_dir / f"{which}.hdf"
     with h5py.File(file_path, "r") as hdf_file:
-        if (n_samples := config["data"].get("n_samples")) is None:
-            theta = np.array(hdf_file["theta"])
-            x = np.array(hdf_file["spectra"])
-            wavelengths = np.array(hdf_file["wavelengths"])
-        else:
-            theta = np.array(hdf_file["theta"][:n_samples])
-            x = np.array(hdf_file["spectra"][:n_samples])
-            wavelengths = np.array(hdf_file["wavelengths"])
 
-    # Define noise levels
+        # Select parameters, flux and wavelengths
+        theta = np.array(hdf_file["theta"][:n_samples])
+        flux = np.array(hdf_file["flux"][:n_samples])
+        wlen = np.array(hdf_file["wlen"])
+
+        # For the test set, add pre-computed noise to flux
+        if which == "test":
+            flux += np.array(hdf_file["noise"][:n_samples])
+
+    # Define noise levels (fixed for the toy dataset)
     noise_levels = 0.5
 
     # Get number of parameters
@@ -152,8 +150,8 @@ def load_toy_dataset(config: dict) -> ArDataset:
     # Create dataset
     return ArDataset(
         theta=torch.from_numpy(theta),
-        x=torch.from_numpy(x),
-        wavelengths=torch.from_numpy(wavelengths),
+        x=torch.from_numpy(flux),
+        wavelengths=torch.from_numpy(wlen),
         noise_levels=noise_levels,
         names=[f"$p_{i}$" for i in range(ndim)],
         ranges=[(-3, 3) for _ in range(ndim)],
