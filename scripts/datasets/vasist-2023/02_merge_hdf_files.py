d81ed85eda60185eae05d7efce49bdc9f2a9f386
Timothy Gebhard <mail@timothygebhard.de>
2023-09-28 17:29:57 +0200

    Rewrite scripts to improve memory efficiency

--- scripts/datasets/vasist-2023/02_merge_hdf_files.py
+++ scripts/datasets/vasist-2023/02_merge_hdf_files.py
@@ -12,76 +12,100 @@ from tqdm import tqdm
 from fm4ar.utils.paths import get_datasets_dir
 
 
-def get_cli_arguments() -> argparse.Namespace:
-    """
-    Get command line arguments.
-    """
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--output-dir",
-        type=str,
-        default="training",
-        help="Name of the output directory (default: training).",
-    )
-    args = parser.parse_args()
-
-    return args
-
-
 if __name__ == "__main__":
 
     script_start = time.time()
     print("\nMERGE HDF FILES\n", flush=True)
 
-    args = get_cli_arguments()
+    # Get command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--target-dir",
+        type=str,
+        default="train",
+        help="Name of the target directory (default: train).",
+    )
+    args = parser.parse_args()
 
-    print("Finding HDF files...", end=" ", flush=True)
-    target_dir = get_datasets_dir() / "vasist-2023" / args.output_dir
+    # Hard-code some constants about the data
+    N_PARAMETERS = 16
+    N_BINS = 947
+
+    # Collect source HDF files
+    print("Finding HDF files...", end=" ")
+    target_dir = get_datasets_dir() / "vasist-2023" / args.target_dir
     file_list = sorted(target_dir.glob("random-seed_*.hdf"))
     print("Done!", flush=True)
-    print(f"Found {len(file_list)} files.\n", flush=True)
+    print(f"Found {len(file_list)} files.\n")
 
-    num_nan_spectra = 0
-    wavelengths = None
-    list_of_thetas = []
-    list_of_spectra = []
-    print("Collecting HDF files:", flush=True)
-    for file_path in tqdm(file_list, ncols=80):
-        with h5py.File(file_path, "r") as hdf_file:
-
-            # Load data from HDF file
-            thetas = np.array(hdf_file["theta"]).astype(float)
-            spectra = np.array(hdf_file["spectra"]).astype(float)
-
-            # Only keep files with at least one spectrum
-            if len(spectra) == 0:
-                continue
-
-            # Exclude spectra with NaNs
-            mask = np.isnan(spectra).any(axis=1)
-            thetas = thetas[~mask]
-            spectra = spectra[~mask]
-            num_nan_spectra += mask.sum()
-
-            # Save data for later
-            list_of_thetas.append(thetas)
-            list_of_spectra.append(spectra)
-            wavelengths = np.array(hdf_file["wavelength"])
-
-    print("\nMerging HDF files...", end=" ", flush=True)
-    thetas = np.concatenate(list_of_thetas)
-    del list_of_thetas
-    spectra = np.concatenate(list_of_spectra)
-    del list_of_spectra
-    file_path = target_dir / "merged.hdf"
-    with h5py.File(file_path, "w") as hdf_file:
-        hdf_file.create_dataset(name="theta", data=thetas, dtype=float)
-        hdf_file.create_dataset(name="spectra", data=spectra, dtype=float)
-        hdf_file.create_dataset(name="wavelengths", data=wavelengths)
+    # Prepare output HDF file
+    print("Preparing output HDF file...", end=" ")
+    dst_file_path = target_dir / "merged.hdf"
+    with h5py.File(dst_file_path, "w") as f:
+        f.create_dataset(
+            name="theta",
+            shape=(0, N_PARAMETERS),
+            maxshape=(None, N_PARAMETERS),
+            dtype=np.float32,
+        )
+        f.create_dataset(
+            name="spectra",
+            shape=(0, N_BINS),
+            maxshape=(None, N_BINS),
+            dtype=np.float32,
+        )
     print("Done!\n", flush=True)
 
-    print(f"Total mumber of spectra: {len(spectra):,}", flush=True)
-    print(f"Excluded due to NaNs:    {num_nan_spectra:,}\n", flush=True)
+    # Keep track of the number of spectra we discard due to NaNs
+    n_dropped = 0
+
+    # Open output HDF file
+    with h5py.File(dst_file_path, "a") as dst:
+
+        # Loop over source HDF files and append data to output HDF file
+        # This should be a lot more memory efficient than loading all data
+        # into memory at once, and then writing it to the output HDF file.
+        print("Collecting HDF files:", flush=True)
+        for src_file_path in tqdm(file_list, ncols=80):
+            with h5py.File(src_file_path, "r") as src:
+
+                # Load data from source HDF file
+                theta = np.array(src["theta"])
+                spectra = np.array(src["spectra"])
+
+                # Only keep files with at least one spectrum
+                if len(spectra) == 0:
+                    continue
+
+                # Exclude spectra with NaNs
+                mask = np.isnan(spectra).any(axis=1)
+                theta = theta[~mask]
+                spectra = spectra[~mask]
+                n = (~mask).sum()
+                n_dropped += mask.sum()
+
+                # Resize datasets in output HDF file
+                dst["theta"].resize(dst["theta"].shape[0] + n, axis=0)
+                dst["spectra"].resize(dst["spectra"].shape[0] + n, axis=0)
+
+                # Write data to output HDF file
+                dst["theta"][-n:] = theta
+                dst["spectra"][-n:] = spectra
+
+                # Copy over wavelengths
+                if "wavelengths" not in dst:
+                    dst.create_dataset(
+                        name="wavelengths",
+                        data=src["wavelengths"][...],
+                        dtype=np.float32,
+                    )
+
+        # Get total number of spectra
+        n_spectra = dst["theta"].shape[0]
+
+    # Print some information about the data
+    print()
+    print(f"Total number of spectra: {n_spectra:,}", flush=True)
+    print(f"Excluded due to NaNs:    {n_dropped:,}", flush=True)
 
     print(f"\nThis took {time.time() - script_start:.1f} seconds.\n")
--- scripts/datasets/vasist-2023/03_select_spectra.py
+++ scripts/datasets/vasist-2023/03_select_spectra.py
@@ -34,22 +34,43 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    train_dir = get_datasets_dir() / "vasist-2023" / args.which
+    # Hard-code some constants about the data
+    N_PARAMETERS = 16
+    N_BINS = 947
+
+    # Define target directory
+    target_dir = get_datasets_dir() / "vasist-2023" / args.which
 
     # Open the HDF file with all the spectra
-    with h5py.File(train_dir / args.output_file_name, "w") as dst:
-        with h5py.File(train_dir / "merged.hdf", "r") as src:
+    with h5py.File(target_dir / args.output_file_name, "w") as dst:
 
-            # Copy over wavelengths; prepare datasets for spectra and theta
+        # Prepare datasets in the output HDF file
+        dst.create_dataset(
+            name="theta",
+            shape=(0, N_PARAMETERS),
+            maxshape=(None, N_PARAMETERS),
+            dtype=np.float32,
+        )
+        dst.create_dataset(
+            name="spectra",
+            shape=(0, N_BINS),
+            maxshape=(None, N_BINS),
+            dtype=np.float32,
+        )
+
+        # Open the HDF file with all the spectra
+        with h5py.File(target_dir / "merged.hdf", "r") as src:
+
+            # # Copy over wavelengths
             dst.create_dataset(
                 name="wavelengths",
                 data=src["wavelengths"][...],
                 dtype=np.float32,
             )
-            spectra = []
-            thetas = []
 
-            # Select spectra that meet the criteria
+            # Select spectra: For this, we loop over chunks of 4096 spectra at
+            # a time (to limit memory consumption) and copy over the ones that
+            # meet the criteria
             print("Selecting spectra:")
             idx = np.arange(0, len(src["spectra"]), 4096)
             for a, b in tqdm(list(zip(idx[:-1], idx[1:])), ncols=80):
@@ -58,23 +79,20 @@ if __name__ == "__main__":
                 mean = np.mean(src["spectra"][a:b], axis=1)
                 mask = (1e-5 <= mean) & (mean <= 1e5)
 
-                spectra.append(np.array(src["spectra"][a:b])[mask])
-                thetas.append(np.array(src["theta"][a:b])[mask])
+                # Select spectra that meet the criteria
+                spectra = np.array(src["spectra"][a:b])[mask]
+                theta = np.array(src["theta"][a:b])[mask]
+                n = len(spectra)
 
-            # Save the selected spectra and thetas
-            print("\nSaving results...", end=" ")
-            dst.create_dataset(
-                name="spectra",
-                data=np.concatenate(spectra, axis=0),
-                dtype=float,
-            )
-            dst.create_dataset(
-                name="theta",
-                data=np.concatenate(thetas, axis=0),
-                dtype=float,
-            )
-            print("Done!\n")
+                # Resize the datasets in the output HDF file
+                dst["spectra"].resize((dst["spectra"].shape[0] + n), axis=0)
+                dst["theta"].resize((dst["theta"].shape[0] + n), axis=0)
 
+                # Save the selected spectra and theta
+                dst["spectra"][-n:] = spectra
+                dst["theta"][-n:] = theta
+
+            # Print some information about how many spectra we selected
             print("Before:", src["spectra"].shape)
             print("After: ", dst["spectra"].shape)
 
