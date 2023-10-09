2d7117211b743843bef374df6b6076c23219ea83
Timothy Gebhard <mail@timothygebhard.de>
2023-10-09 15:16:06 +0200

    Add option to fix parameters to their `THETA_0` values

--- scripts/datasets/vasist-2023/01_simulate_spectra.py
+++ scripts/datasets/vasist-2023/01_simulate_spectra.py
@@ -9,7 +9,7 @@ import h5py
 import numpy as np
 from tqdm import tqdm
 
-from fm4ar.datasets.vasist_2023.prior import Prior
+from fm4ar.datasets.vasist_2023.prior import NAMES, Prior, THETA_0
 from fm4ar.datasets.vasist_2023.simulation import Simulator
 from fm4ar.utils.paths import get_datasets_dir
 
@@ -27,6 +27,17 @@ def get_cli_arguments() -> argparse.Namespace:
         help="Number of spectra to generate.",
     )
     parser.add_argument(
+        "--parameters",
+        type=str,
+        nargs="+",
+        default=None,
+        help=(
+            "Parameters to sample from the prior. If None, all 16 parameters "
+            "will be sampled (default). Parameters that are not sampled will "
+            "be set to their 'default' values (from THETA_0)."
+        )
+    )
+    parser.add_argument(
         "--random-seed",
         type=int,
         default=42,
@@ -42,6 +53,7 @@ def get_cli_arguments() -> argparse.Namespace:
         "--resolution",
         type=int,
         default=1_000,
+        choices=[400, 1_000],
         help=(
             "Spectral resolution (R = λ/∆λ). Default: 1000. Alternative: 400."
         ),
@@ -49,8 +61,8 @@ def get_cli_arguments() -> argparse.Namespace:
     parser.add_argument(
         "--output-dir",
         type=str,
-        default="training",
-        help="Name of the output directory (default: training).",
+        default="output",
+        help="Name of the output directory (default: 'output').",
     )
     parser.add_argument(
         "--time-limit",
@@ -74,6 +86,12 @@ if __name__ == "__main__":
     prior = Prior(random_seed=effective_random_seed)
     simulator = Simulator(R=args.resolution, time_limit=args.time_limit)
 
+    # Define mask for parameters to fix to their THETA_0 values
+    if args.parameters is None:
+        mask = np.zeros(len(NAMES), dtype=bool)
+    else:
+        mask = np.array([p not in args.parameters for p in NAMES])
+
     # Prepare lists to store the results
     wavelengths = np.empty(0)
     list_of_thetas = []
@@ -82,7 +100,12 @@ if __name__ == "__main__":
     # Run the simulation
     print("Simulating spectra:", flush=True)
     for _ in tqdm(range(args.n_spectra), ncols=80, total=args.n_spectra):
+
+        # Sample parameters from prior and set fixed parameters to THETA_0
         theta = prior.sample()
+        theta[mask] = THETA_0[mask]
+
+        # Simulate spectrum and store results if successful
         result = simulator(theta)
         if result is not None:
             wavelengths, spectrum = result
