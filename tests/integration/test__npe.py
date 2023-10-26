023a7957af257128d66e681aea8af39ba21e38fd
Timothy Gebhard <mail@timothygebhard.de>
2023-10-26 22:08:54 +0200

    Update unit tests

--- tests/integration/test__npe.py
+++ tests/integration/test__npe.py
@@ -9,13 +9,22 @@ from fm4ar.models.build_model import build_model
 
 def test__npe_model__glasflow() -> None:
 
+    # Define sizes
+    batch_size = 5
+    theta_dim = 16
+    n_bins = 947
+    n_features = 3
+
     # Define a dummy configuration
     config = {
         "model": {
             "model_type": "npe",
-            "theta_dim": 7,
-            "context_dim": 403,
+            "theta_dim": theta_dim,
+            "context_dim": (n_bins, n_features),
             "context_embedding_kwargs": {
+                "00_DropFeatures": {
+                    "model_type": "DropFeatures",
+                },
                 "01_DenseResidualNet": {
                     "model_type": "DenseResidualNet",
                     "kwargs": {
@@ -47,32 +56,41 @@ def test__npe_model__glasflow() -> None:
     model = build_model(config=config)
 
     # Define dummy theta and context
-    theta = torch.randn(10, 7)
-    context = torch.randn(10, 403)
+    theta = torch.randn(batch_size, theta_dim)
+    context = torch.randn(batch_size, n_bins, n_features)
 
     # Make sure we sample from it
     samples = model.sample_batch(context=context)
-    assert samples.shape == (10, 7)
+    assert samples.shape == (batch_size, theta_dim)
 
     # Make sure we can compute the log probability
     log_prob = model.log_prob_batch(theta=theta, context=context)
-    assert log_prob.shape == (10,)
+    assert log_prob.shape == (batch_size,)
 
     # Make sure we can sample and compute the log probability
     samples, log_prob = model.sample_and_log_prob_batch(context=context)
-    assert samples.shape == (10, 7)
-    assert log_prob.shape == (10,)
+    assert samples.shape == (batch_size, theta_dim)
+    assert log_prob.shape == (batch_size,)
 
 
 def test__npe_model__normflows() -> None:
+
+    # Define sizes
+    batch_size = 5
+    theta_dim = 16
+    n_bins = 947
+    n_features = 3
 
     # Define a dummy configuration
     config = {
         "model": {
             "model_type": "npe",
-            "theta_dim": 7,
-            "context_dim": 403,
+            "theta_dim": theta_dim,
+            "context_dim": (n_bins, n_features),
             "context_embedding_kwargs": {
+                "00_DropFeatures": {
+                    "model_type": "DropFeatures",
+                },
                 "01_DenseResidualNet": {
                     "model_type": "DenseResidualNet",
                     "kwargs": {
@@ -104,18 +122,18 @@ def test__npe_model__normflows() -> None:
     model = build_model(config=config)
 
     # Define dummy theta and context
-    theta = torch.randn(10, 7)
-    context = torch.randn(10, 403)
+    theta = torch.randn(batch_size, theta_dim)
+    context = torch.randn(batch_size, n_bins, n_features)
 
     # Make sure we sample from it
     samples = model.sample_batch(context=context)
-    assert samples.shape == (10, 7)
+    assert samples.shape == (batch_size, theta_dim)
 
     # Make sure we can compute the log probability
     log_prob = model.log_prob_batch(theta=theta, context=context)
-    assert log_prob.shape == (10,)
+    assert log_prob.shape == (batch_size,)
 
     # Make sure we can sample and compute the log probability
     samples, log_prob = model.sample_and_log_prob_batch(context=context)
-    assert samples.shape == (10, 7)
-    assert log_prob.shape == (10,)
+    assert samples.shape == (batch_size, theta_dim)
+    assert log_prob.shape == (batch_size,)
