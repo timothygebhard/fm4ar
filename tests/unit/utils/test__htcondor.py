3eb5aa92b6f2fc3e858a05d79c28617877f2980e
Timothy Gebhard <mail@timothygebhard.de>
2024-02-13 15:46:01 +0100

    Fix unit tests

--- tests/unit/utils/test__htcondor.py
+++ tests/unit/utils/test__htcondor.py
@@ -8,6 +8,7 @@ from pathlib import Path
 import pytest
 
 from fm4ar.utils.htcondor import (
+    CondorSettings,
     check_if_on_login_node,
     copy_logfiles,
     create_submission_file,
@@ -19,6 +20,7 @@ def test__check_if_on_login_node(monkeypatch: pytest.MonkeyPatch) -> None:
     Test `fm4ar.utils.htcondor.check_if_on_login_node()`.
     """
 
+    # noinspection PyArgumentList
     with monkeypatch.context() as context:
         context.setattr(socket, "gethostname", lambda: "login")
         with pytest.raises(SystemExit) as excinfo:
@@ -55,14 +57,14 @@ def test__create_submission_file(tmp_path: Path) -> None:
     # Case 1: Invalid experiment directory
     with pytest.raises(FileNotFoundError) as file_not_found_error:
         create_submission_file(
-            condor_settings={"arguments": ""},
+            condor_settings=CondorSettings(arguments=""),
             experiment_dir=tmp_path / "does_not_exist",
         )
     assert "Experiment directory does not exist" in str(file_not_found_error)
 
     # Case 2: Arguments as string
     file_path = create_submission_file(
-        condor_settings={"arguments": "arguments as string"},
+        condor_settings=CondorSettings(arguments="arguments as string"),
         experiment_dir=tmp_path,
         file_name="run.sub",
     )
@@ -70,10 +72,10 @@ def test__create_submission_file(tmp_path: Path) -> None:
 
     # Case 3: Arguments as list
     file_path = create_submission_file(
-        condor_settings={
-            "arguments": ["arguments", "as", "list"],
-            "num_gpus": 1,
-        },
+        condor_settings=CondorSettings(
+            arguments=["arguments", "as", "list"],
+            num_gpus=1,
+        ),
         experiment_dir=tmp_path,
         file_name="run.sub",
     )
