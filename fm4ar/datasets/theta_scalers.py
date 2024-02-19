9c6f5f723ba73138104ed07abb1c0306bb908985
Timothy Gebhard <mail@timothygebhard.de>
2024-02-19 20:43:00 +0100

    Add theta_scalers.py and unit tests

---
+++ fm4ar/datasets/theta_scalers.py
@@ -0,0 +1,153 @@
+"""
+Methods for scaling the target parameters `theta`.
+
+Note: The `theta` scaling is different from the `data_transforms`, as
+the it remains fixed for the entire training process while the data
+transforms can change between different stages of training.
+"""
+
+from abc import ABC, abstractmethod
+from typing import Any
+
+import numpy as np
+
+from fm4ar.datasets.vasist_2023.prior import LOWER, UPPER
+
+
+class ThetaScaler(ABC):
+    """
+    Base class for all theta scalers.
+    """
+
+    @abstractmethod
+    def forward(self, x: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
+        raise NotImplementedError  # pragma: no cover
+
+    @abstractmethod
+    def inverse(self, x: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
+        raise NotImplementedError  # pragma: no cover
+
+
+class IdentityScaler(ThetaScaler):
+    """
+    Identity scaler for `theta`.
+    """
+
+    def forward(self, x: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
+        return x
+
+    def inverse(self, x: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
+        return x
+
+
+class MeanStdScaler(ThetaScaler):
+    """
+    Scale `theta` by subtracting the mean and dividing by the std. dev.
+    """
+
+    def __init__(
+        self,
+        mean: np.ndarray,
+        std: np.ndarray,
+    ) -> None:
+
+        super().__init__()
+
+        self.mean = mean
+        self.std = std
+
+    def forward(self, x: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
+        x["theta"] = (x["theta"] - self.mean) / self.std
+        return x
+
+    def inverse(self, x: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
+        x["theta"] = x["theta"] * self.std + self.mean
+        return x
+
+
+class MinMaxScaler(ThetaScaler):
+    """
+    Scale `theta` by mapping it into the interval [0, 1].
+    """
+
+    def __init__(
+        self,
+        minimum: np.ndarray,
+        maximum: np.ndarray,
+    ) -> None:
+
+        super().__init__()
+
+        self.minimum = minimum
+        self.maximum = maximum
+        self.difference = self.maximum - self.minimum
+
+    def forward(self, x: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
+        x["theta"] = (x["theta"] - self.minimum) / self.difference
+        return x
+
+    def inverse(self, x: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
+        x["theta"] = x["theta"] * self.difference + self.minimum
+        return x
+
+
+def get_theta_scaler(config: dict[str, Any]) -> ThetaScaler:
+    """
+    Get the scaler for theta specified in the given `config` (which is
+    only the config for the feature scaler for theta, not the entire
+    experiment config).
+    """
+
+    # Case 1: No feature scaling defined for theta
+    if not config:
+        return IdentityScaler()
+
+    # Case 2: Feature scaling defined
+    scaler: ThetaScaler
+    method = config["method"]
+    kwargs = config.get("kwargs", {})
+    match method:
+        case "mean_std" | "MeanStdScaler":
+            mean, std = get_mean_and_std(**kwargs)
+            scaler = MeanStdScaler(mean=mean, std=std)
+        case "min_max" | "MinMaxScaler":
+            minimum, maximum = get_min_and_max(**kwargs)
+            scaler = MinMaxScaler(minimum=minimum, maximum=maximum)
+        case "identity" | "IdentityScaler":
+            scaler = IdentityScaler()
+        case _:
+            raise ValueError(f"Unknown feature scaling method: {method}")
+
+    return scaler
+
+
+def get_mean_and_std(dataset: str) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Get the mean and standard deviation of the target parameters.
+    """
+
+    if dataset == "vasist-2023":
+        a = np.array(LOWER)
+        b = np.array(UPPER)
+        mean = (a + b) / 2
+        std = np.sqrt(1 / 12 * (b - a) ** 2)
+
+    else:
+        raise ValueError(f"Unknown dataset: {dataset}")
+
+    return mean, std
+
+
+def get_min_and_max(dataset: str) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Get the minimum and maximum of the target parameters.
+    """
+
+    if dataset == "vasist-2023":
+        minimum = np.array(LOWER)
+        maximum = np.array(UPPER)
+
+    else:
+        raise ValueError(f"Unknown dataset: {dataset}")
+
+    return minimum, maximum
