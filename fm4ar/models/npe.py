ae61f65b74aca3c6916b1f90e3e1c0a37d3404a7
Timothy Gebhard <mail@timothygebhard.de>
2024-02-19 20:58:02 +0100

    Major refactoring of the NPE-related methods; add integration test

---
+++ fm4ar/models/npe.py
@@ -0,0 +1,208 @@
+"""
+Methods for neural posterior estimation (NPE) models.
+"""
+
+from functools import lru_cache
+from typing import Any
+
+import torch
+from torch import nn as nn
+
+from fm4ar.models.base import Base
+from fm4ar.nn.embedding_nets import create_embedding_net
+from fm4ar.nn.flows import FlowWrapper, create_flow_wrapper
+from fm4ar.utils.misc import freeze_args
+
+
+class NPEModel(Base):
+
+    # Add type hint for the network
+    network: "NPENetwork"
+
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+
+    def initialize_network(self) -> None:
+        """
+        Initialize the NPE network.
+        """
+
+        self.network = create_npe_network(model_config=self.config["model"])
+
+    def log_prob_batch(
+        self,
+        theta: torch.Tensor,
+        context: dict[str, torch.Tensor],
+    ) -> torch.Tensor:
+        """
+        Compute the log probability of the given `theta`.
+        """
+
+        self.network.eval()
+
+        context_embedding = self.network.get_context_embedding(context)
+        log_prob = self.network.flow_wrapper.log_prob(
+            theta=theta,
+            context=context_embedding,
+        )
+
+        return log_prob
+
+    def sample_batch(
+        self,
+        context: dict[str, torch.Tensor],
+        num_samples: int = 1,
+    ) -> torch.Tensor:
+        """
+        Sample from the model and return the samples. If `context` is
+        None, we need to specify the number of samples to draw;
+        otherwise, we assume that the number of samples is the same as
+        the batch size of `context`.
+        """
+
+        self.network.eval()
+
+        context_embedding = self.network.get_context_embedding(context)
+        samples = self.network.flow_wrapper.sample(
+            num_samples=num_samples,
+            context=context_embedding,
+        )
+
+        return samples
+
+    def sample_and_log_prob_batch(
+        self,
+        context: dict[str, torch.Tensor],
+        num_samples: int = 1,
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        """
+        Sample from the model and return the samples and their log
+        probabilities. If `context` is None, we need to specify the
+        number of samples to draw; otherwise, we assume that the
+        number of samples is the same as the batch size of `context`.
+        """
+
+        self.network.eval()
+
+        context_embedding = self.network.get_context_embedding(context)
+        samples, log_prob = self.network.flow_wrapper.sample_and_log_prob(
+            num_samples=num_samples,
+            context=context_embedding,
+        )
+
+        return samples, log_prob
+
+    def loss(
+        self,
+        theta: torch.Tensor,
+        context: dict[str, torch.Tensor],
+        **kwargs: Any,
+    ) -> torch.Tensor:
+        """
+        Compute the loss for the given `theta` and `context` (i.e.,
+        the mean negative log probability).
+        """
+
+        return torch.Tensor(-self.network(theta=theta, context=context).mean())
+
+
+class NPENetwork(nn.Module):
+    """
+    This class is a wrapper that combines an embedding net for the
+    context with the actual discrete flow that models the posterior.
+    """
+
+    def __init__(
+        self,
+        context_embedding_net: nn.Module,
+        flow_wrapper: FlowWrapper,
+    ) -> None:
+        """
+        Initialize a NPENetwork instance.
+
+        Args:
+            context_embedding_net: The context embedding network.
+            flow_wrapper: Wrapped version of the actual (discrete) flow.
+        """
+
+        super().__init__()
+
+        self.context_embedding_net = context_embedding_net
+        self.flow_wrapper = flow_wrapper
+
+    @freeze_args
+    @lru_cache(maxsize=1)
+    def get_context_embedding(
+        self,
+        context: dict[str, torch.Tensor],
+    ) -> torch.Tensor:
+        """
+        Get the embedding of the context. We wrap this in a separate
+        method to allow caching the last result. Since the context is
+        a dictionary, we need to convert it to a `frozendict` first to
+        allow caching with the `lru_cache` decorator.
+        """
+
+        return torch.Tensor(self.context_embedding_net(context))
+
+    def forward(
+        self,
+        theta: torch.Tensor,
+        context: dict[str, torch.Tensor],
+    ) -> torch.Tensor:
+        """
+        Forward pass through the model. This returns the log probability
+        of `theta` given the `context`, which is what we need when we
+        train the model using the NPE loss function.
+        """
+
+        # Get the embedding of the context (with caching)
+        context_embedding = self.get_context_embedding(context=context)
+
+        return self.flow_wrapper.log_prob(
+            theta=theta,
+            context=context_embedding,
+        )
+
+
+def create_npe_network(model_config: dict) -> NPENetwork:
+    """
+    Create a `NPENetwork` instance from the given model configuration.
+
+    Args:
+        model_config: Keyword arguments specifying the model, that is,
+            the "model" section of the configuration file.
+
+    Returns:
+        The NPE network wrapper.
+    """
+
+    # Extract dimensions of `theta` and `context`
+    # Note: These are *not* in the config file, but are added by the
+    # `prepare_new()` method that is called at the start of training.
+    dim_theta = int(model_config["dim_theta"])
+    dim_context = int(model_config["dim_context"])
+
+    # Construct an embedding network for the context
+    context_embedding_net, dim_embedded_context = create_embedding_net(
+        input_shape=(dim_context,),
+        block_configs=model_config["context_embedding_net"],
+        supports_dict_input=True,
+    )
+
+    # Construct the actual discrete normalizing flow
+    # We use a `FlowWrapper` to wrap the actual flow and handle the different
+    # conventions used by different flows libraries (normflows vs. glasflow)
+    flow_wrapper = create_flow_wrapper(
+        dim_theta=dim_theta,
+        dim_context=dim_embedded_context,
+        flow_wrapper_config=model_config["flow_wrapper"],
+    )
+
+    # Bundle everything into a `NPENetwork` wrapper
+    network = NPENetwork(
+        flow_wrapper=flow_wrapper,
+        context_embedding_net=context_embedding_net,
+    )
+
+    return network
--- tests/integration/test__npe.py
+++ tests/integration/test__npe.py
@@ -2,138 +2,251 @@
 Test that we can construct a flow matching model from a configuration.
 """
 
+from shutil import copyfile
+from pathlib import Path
+
+import h5py
+import numpy as np
+import pytest
 import torch
+from deepdiff import DeepDiff
 
 from fm4ar.models.build_model import build_model
+from fm4ar.utils.config import load_config
+from fm4ar.training.preparation import prepare_new, prepare_resume
+from fm4ar.training.stages import initialize_stage, train_stages
+from fm4ar.training.train_validate import (
+    move_batch_to_device,
+    train_epoch,
+    validate_epoch,
+)
+from fm4ar.utils.paths import get_experiments_dir
+from fm4ar.utils.tracking import RuntimeLimits
 
 
-def test__npe_model__glasflow() -> None:
+# Define some constants for the mock data
+N_TOTAL = 22  # number of samples in the mock dataset
+N_LOAD = 20  # number of samples to load from the mock dataset
+BATCH_SIZE = 5  # batch size for the mock data
+DIM_THETA = 16  # required to use Vasist-2023 feature scaler
+N_BINS = 39  # number of bins in the mock data
 
-    # Define sizes
-    batch_size = 5
-    theta_dim = 16
-    n_bins = 947
-    n_features = 3
 
-    # Define a dummy configuration
-    config = {
-        "model": {
-            "model_type": "npe",
-            "theta_dim": theta_dim,
-            "context_dim": (n_bins, n_features),
-            "context_embedding_kwargs": {
-                "00_DropFeatures": {
-                    "model_type": "DropFeatures",
-                },
-                "01_DenseResidualNet": {
-                    "model_type": "DenseResidualNet",
-                    "kwargs": {
-                        "hidden_dims": [256, 128],
-                        "activation": "elu",
-                        "output_dim": 128,
-                        "dropout": 0,
-                        "batch_norm": False,
-                    },
-                },
-            },
-            "posterior_kwargs": {
-                "flow_library": "glasflow",
-                "num_flow_steps": 14,
+@pytest.fixture
+def experiment_dir(tmp_path: Path) -> Path:
+    """
+    Create a dummy experiment directory and return the path to it.
+    """
+
+    # Create the dummy experiment directory
+    experiment_dir = tmp_path / "dummy_experiment_npe"
+    experiment_dir.mkdir()
+
+    # Copy over the template configuration
+    template_dir = get_experiments_dir() / "npe-template"
+    copyfile(
+        template_dir / "config.yaml",
+        experiment_dir / "config.yaml",
+    )
+
+    return experiment_dir
+
+
+@pytest.fixture
+def path_to_dummy_dataset(tmp_path: Path) -> Path:
+    """
+    Create a dummy dataset for testing and return the path to it.
+    """
+
+    file_path = tmp_path / "dummy_dataset.hdf"
+
+    # Create a dummy dataset
+    with h5py.File(file_path, "w") as f:
+        f.create_dataset("theta", data=np.random.rand(N_TOTAL, DIM_THETA))
+        f.create_dataset("wlen", data=np.random.rand(N_TOTAL, N_BINS))
+        f.create_dataset("flux", data=np.random.rand(N_TOTAL, N_BINS))
+
+    return file_path
+
+
+# We run for both flow libraries
+@pytest.mark.parametrize(
+    "flow_wrapper_config",
+    [
+        {
+            "flow_library": "glasflow",
+            "kwargs": {
+                "num_flow_steps": 3,
                 "base_transform_type": "rq-coupling",
                 "base_transform_kwargs": {
-                    "hidden_dim": 512,
-                    "num_transform_blocks": 3,
-                    "activation": "elu",
+                    "hidden_dim": 64,
+                    "num_transform_blocks": 2,
+                    "activation": "ELU",
                     "dropout_probability": 0.1,
-                    "batch_norm": False,
-                    "num_bins": 10
+                    "batch_norm": True,
+                    "num_bins": 10,
                 },
             },
-        }
-    }
-
-    # Build the model
-    model = build_model(config=config)
-
-    # Define dummy theta and context
-    theta = torch.randn(batch_size, theta_dim)
-    context = torch.randn(batch_size, n_bins, n_features)
-
-    # Make sure we sample from it
-    samples = model.sample_batch(context=context)
-    assert samples.shape == (batch_size, theta_dim)
-
-    # Make sure we can compute the log probability
-    log_prob = model.log_prob_batch(theta=theta, context=context)
-    assert log_prob.shape == (batch_size,)
-
-    # Make sure we can sample and compute the log probability
-    samples, log_prob = model.sample_and_log_prob_batch(context=context)
-    assert samples.shape == (batch_size, theta_dim)
-    assert log_prob.shape == (batch_size,)
-
-
-def test__npe_model__normflows() -> None:
-
-    # Define sizes
-    batch_size = 5
-    theta_dim = 16
-    n_bins = 947
-    n_features = 3
-
-    # Define a dummy configuration
-    config = {
-        "model": {
-            "model_type": "npe",
-            "theta_dim": theta_dim,
-            "context_dim": (n_bins, n_features),
-            "context_embedding_kwargs": {
-                "00_DropFeatures": {
-                    "model_type": "DropFeatures",
-                },
-                "01_DenseResidualNet": {
-                    "model_type": "DenseResidualNet",
-                    "kwargs": {
-                        "hidden_dims": [256, 128],
-                        "activation": "elu",
-                        "output_dim": 128,
-                        "dropout": 0,
-                        "batch_norm": False,
-                    },
-                },
-            },
-            "posterior_kwargs": {
-                "flow_library": "normflows",
-                "num_flow_steps": 14,
+        },
+        {
+            "flow_library": "normflows",
+            "kwargs": {
+                "num_flow_steps": 3,
                 "base_transform_type": "rq-coupling",
                 "base_transform_kwargs": {
-                    "num_blocks": 3,
-                    "num_hidden_channels": 512,
+                    "num_blocks": 2,
+                    "num_hidden_channels": 64,
                     "num_bins": 10,
                     "tail_bound": 10,
-                    "activation": "elu",
-                    "dropout_probability": 0.1
-                }
-            }
-        }
-    }
+                    "activation": "ELU",
+                    "dropout_probability": 0.1,
+                },
+            },
+        },
+    ],
+)
+@pytest.mark.integration_test
+def test__npe_model(
+    flow_wrapper_config: dict,
+    experiment_dir: Path,
+    path_to_dummy_dataset: Path,
+) -> None:
+    """
+    Integration test to check that we can build an NPE model from the
+    template configuration and send some mock data through it.
+    """
 
-    # Build the model
-    model = build_model(config=config)
+    # Set the random seed for reproducibility
+    torch.manual_seed(0)
 
-    # Define dummy theta and context
-    theta = torch.randn(batch_size, theta_dim)
-    context = torch.randn(batch_size, n_bins, n_features)
+    # Read in template configuration (which was copied to the experiment dir)
+    config = load_config(experiment_dir=experiment_dir)
 
-    # Make sure we sample from it
+    # Overwrite the dataset section
+    # This should give us 3 training batches and 1 validation batch
+    config["dataset"]["file_path"] = path_to_dummy_dataset.as_posix()
+    config["dataset"]["train_fraction"] = 0.75
+    config["dataset"]["n_samples"] = N_LOAD
+
+    # Overwrite the the flow_wrapper configuration
+    config["model"]["flow_wrapper"] = flow_wrapper_config
+
+    # Prepare the model and the dataset
+    model, dataset = prepare_new(experiment_dir=experiment_dir, config=config)
+
+    # Select the first stage; make sure config is suitable for testing
+    stage_config = next(iter(config["training"].values()))
+    stage_config["batch_size"] = BATCH_SIZE
+    stage_config["logprob_epochs"] = 3
+    stage_config["use_amp"] = False
+
+    # Initialize the stage
+    train_loader, valid_loader = initialize_stage(
+        model=model,
+        dataset=dataset,
+        num_workers=0,
+        resume=False,
+        stage_config=stage_config,
+        stage_number=1,
+    )
+
+    # Get a batch of mock data
+    batch = next(iter(train_loader))
+    theta, context = move_batch_to_device(batch, model.device)
+
+    # Send the mock data through the model
+    loss = model.loss(theta=theta, context=context)
+    assert np.isfinite(loss.item())
+
+    # Check that we can get the context embedding
+    expected_embedding_dim = config["model"]["context_embedding_net"][-1][
+        "kwargs"
+    ]["output_dim"]
+    context_embedding = model.network.get_context_embedding(context=context)
+    assert context_embedding.shape == (BATCH_SIZE, expected_embedding_dim)
+
+    # Check that we can train and validate manually for two epochs.
+    # We train for _two_ epochs to be sure that `validate_epoch()` goes into
+    # the branch where we compute the average log probability.
+    for epoch in range(1, 3):
+
+        # Manually set the model epoch
+        model.epoch = epoch
+
+        # This should be 3 batches
+        train_loss = train_epoch(
+            model=model,
+            dataloader=train_loader,
+            stage_config=stage_config,
+        )
+        assert np.isfinite(train_loss)
+
+        # This should be 1 batch
+        avg_log_prob: float | None
+        val_loss, avg_log_prob = validate_epoch(
+            model=model,
+            dataloader=train_loader,
+            stage_config=stage_config,
+        )
+        assert np.isfinite(val_loss)
+        if epoch == 1:
+            assert avg_log_prob is not None
+            assert np.isfinite(avg_log_prob)
+        if epoch == 2:
+            assert avg_log_prob is None
+
+    # Check that we can train for two more epochs using the .train() method
+    model.train(
+        train_loader=train_loader,
+        valid_loader=valid_loader,
+        runtime_limits=RuntimeLimits(max_epochs_total=4),
+        stage_config=stage_config,
+    )
+
+    # Check that we can sample from the model
     samples = model.sample_batch(context=context)
-    assert samples.shape == (batch_size, theta_dim)
+    assert samples.shape == (BATCH_SIZE, DIM_THETA)
 
-    # Make sure we can compute the log probability
-    log_prob = model.log_prob_batch(theta=theta, context=context)
-    assert log_prob.shape == (batch_size,)
+    # Check that we can get the log probability of the samples
+    log_prob = model.log_prob_batch(theta=samples, context=context)
+    assert log_prob.shape == (BATCH_SIZE,)
 
-    # Make sure we can sample and compute the log probability
+    # Check that we can sample and get the log probability in one go
     samples, log_prob = model.sample_and_log_prob_batch(context=context)
-    assert samples.shape == (batch_size, theta_dim)
-    assert log_prob.shape == (batch_size,)
+    assert samples.shape == (BATCH_SIZE, DIM_THETA)
+    assert log_prob.shape == (BATCH_SIZE,)
+
+    # Check that we can save the model
+    file_path = model.save_model()
+    assert file_path is not None and file_path.exists()
+
+    # Check that we can load the model and recover the same configuration
+    restored_model = build_model(file_path=file_path)
+    assert restored_model.config == model.config
+    assert not DeepDiff(
+        restored_model.network.state_dict().keys(),
+        model.network.state_dict().keys(),
+    )
+    for key in model.network.state_dict().keys():
+        assert torch.allclose(
+            model.network.state_dict()[key],
+            restored_model.network.state_dict()[key],
+        )
+
+    # Check that we can save a snapshot
+    snapshot_file_path = model.save_snapshot()
+    assert snapshot_file_path is not None and snapshot_file_path.exists()
+
+    # Check that we can use prepare_resume()
+    resumed_model, resumed_dataset = prepare_resume(
+        experiment_dir=experiment_dir,
+        checkpoint_name="model__latest.pt",
+        config=config,
+    )
+    assert resumed_model.config == model.config
+    assert np.allclose(dataset.theta, resumed_dataset.theta)
+
+    # Check that we can use train_stage()
+    done = train_stages(model=model, dataset=dataset)
+    assert done
+    assert model.epoch == 5
