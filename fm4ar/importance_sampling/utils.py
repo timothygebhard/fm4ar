21212954b91c3bb56a948c056b958e8ae49c0aa6
Timothy Gebhard <mail@timothygebhard.de>
2024-03-14 20:07:26 +0100

    Improve weight calculation

--- fm4ar/importance_sampling/utils.py
+++ fm4ar/importance_sampling/utils.py
@@ -2,6 +2,8 @@
 Utility functions for importance sampling.
 """
 
+from warnings import warn
+
 import numpy as np
 
 
@@ -26,8 +28,19 @@ def compute_is_weights(
     # Compute the raw weights
     raw_weights = likelihoods * prior_values / probs
 
-    # Normalize the weights
-    normalized_weights = raw_weights * len(raw_weights) / np.sum(raw_weights)
+    # In case any weights are NaN of Inf, set them to 0
+    raw_weights = np.nan_to_num(raw_weights, nan=0.0, posinf=0.0, neginf=0.0)
+
+    # Normalize the weights such that they sum to the number of samples.
+    # If the sum of the raw weights is zero, issue a warning and set the
+    # normalized weights to a uniform distribution.
+    if np.sum(raw_weights) == 0:
+        warn(UserWarning("All raw_weights are zero!"))
+        normalized_weights = np.ones_like(raw_weights) / len(raw_weights)
+    else:
+        normalized_weights = (  # fmt: off
+            raw_weights * len(raw_weights) / np.sum(raw_weights)
+        )  # fmt: on
 
     return raw_weights, normalized_weights
 
