fc1a94749cf21ed346e46e69aa78f7fe09e11256
Timothy Gebhard <mail@timothygebhard.de>
2024-03-15 17:20:45 +0100

    Add `clip_and_normalize_weights()`

--- fm4ar/importance_sampling/utils.py
+++ fm4ar/importance_sampling/utils.py
@@ -6,6 +6,51 @@ import numpy as np
 from scipy.special import logsumexp
 
 
+def clip_and_normalize_weights(
+    raw_log_weights: np.ndarray,
+    percentile: float | None = None,
+) -> np.ndarray:
+    """
+    Clip and normalize the raw log-weights.
+
+    Args:
+        raw_log_weights: Raw log-weights.
+        percentile: (Upper) percentile for clipping. If `None`, no
+            clipping is applied (this is the default).
+
+    Returns:
+        normalized_weights: Normalized importance sampling weights.
+    """
+
+    # Clip the raw log-weights, if desired
+    if percentile is not None:
+        threshold = np.percentile(raw_log_weights, percentile)
+        clipped_weights = np.clip(raw_log_weights, None, threshold)
+    else:
+        clipped_weights = raw_log_weights
+
+    # Normalize the clipped log-weights
+    # In "normal" space, we normalize the raw weights such that they sum to
+    # the number of samples, that is:
+    #   n_i = w_i * N / sum(w_i) ,
+    # where N is the number of samples. We now only have access to the log-
+    # weights log(w_i), so we use the following equivalent expression:
+    #   n_i = exp{ log(N) + log(w_i) - LSE(log(w_i)) } ,
+    # where the LSE is the log-sum-exp function:
+    #   LSE(x)) = log{ sum(exp(x)) } .
+    # Using the log-sum-exp trick, we can compute the LSE in a numerically
+    # stable way. This allows to compute the normalized weights without
+    # ever needing access to the (raw) likelihoods, priors, or proposals.
+    # For more details about the log-sum-exp trick, see, e.g.,:
+    # https://gregorygundersen.com/blog/2020/02/09/log-sum-exp/
+    N = len(raw_log_weights)
+    normalized_weights = np.exp(
+        np.log(N) + clipped_weights - logsumexp(clipped_weights)
+    )
+
+    return np.array(normalized_weights)
+
+
 def compute_is_weights(
     log_likelihoods: np.ndarray,
     log_prior_values: np.ndarray,
@@ -31,23 +76,10 @@ def compute_is_weights(
     # However, L_i is usually very small, so we use the log-weights instead.
     raw_log_weights = log_likelihoods + log_prior_values - log_probs
 
-    # Normalize the weights
-    # In "normal" space, we normalize the raw weights such that they sum to
-    # the number of samples, that is:
-    #   n_i = w_i * N / sum(w_i) ,
-    # where N is the number of samples. We now only have access to the log-
-    # weights log(w_i), so we use the following equivalent expression:
-    #   n_i = exp{ log(N) + log(w_i) - LSE(log(w_i)) } ,
-    # where the LSE is the log-sum-exp function:
-    #   LSE(x_i)) = log{ sum(exp(x_i)) } .
-    # Using the log-sum-exp trick, we can compute the LSE in a numerically
-    # stable way. This allows to compute the normalized weights without
-    # ever needing access to the (raw) likelihoods, priors, or proposals.
-    # For more details about the log-sum-exp trick, see, e.g.,:
-    # https://gregorygundersen.com/blog/2020/02/09/log-sum-exp/
-    N = len(raw_log_weights)
-    normalized_weights = np.exp(
-        np.log(N) + raw_log_weights - logsumexp(raw_log_weights)
+    # Normalize the raw log-weights (by default without clipping)
+    normalized_weights = clip_and_normalize_weights(
+        raw_log_weights=raw_log_weights,
+        percentile=None,
     )
 
     return raw_log_weights, normalized_weights
