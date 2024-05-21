ca405a2cb779014292557bd4d1baad41070fa33a
Timothy Gebhard <mail@timothygebhard.de>
2024-05-21 14:21:57 +0200

    Update `suppress_output()` for more control over what gets suppressed

--- fm4ar/utils/misc.py
+++ fm4ar/utils/misc.py
@@ -3,7 +3,12 @@ Miscellaneous functions that don't fit anywhere else.
 """
 
 import os
-from contextlib import redirect_stdout, contextmanager
+from contextlib import (
+    contextmanager,
+    nullcontext,
+    redirect_stdout,
+    redirect_stderr,
+)
 from functools import wraps
 from typing import Any, Callable, Generator
 
@@ -36,7 +41,22 @@ def freeze_args(func: Callable) -> Callable:
 
 
 @contextmanager
-def suppress_output() -> Generator[None, None, None]:
+def suppress_output(
+    stdout: bool = True,
+    stderr: bool = False,
+) -> Generator[None, None, None]:
+    """
+    Context manager to suppress the output to stdout and/or stderr.
+
+    Args:
+        stdout: Whether to suppress the output to stdout.
+        stderr: Whether to suppress the output to stderr.
+    """
+
+    # Note: `with suppress_stdout and suppress_stderr` does *not* work
     with open(os.devnull, "w") as null:
-        with redirect_stdout(null):
-            yield
+        suppress_stdout = redirect_stdout(null) if stdout else nullcontext()
+        suppress_stderr = redirect_stderr(null) if stderr else nullcontext()
+        with suppress_stdout:
+            with suppress_stderr:
+                yield
--- tests/unit/utils/test__misc.py
+++ tests/unit/utils/test__misc.py
@@ -2,13 +2,14 @@
 Unit tests for `fm4ar.utils.misc`.
 """
 
+import sys
 from functools import lru_cache
 from typing import Any
 
 import pytest
 from frozendict import frozendict
 
-from fm4ar.utils.misc import freeze_args
+from fm4ar.utils.misc import freeze_args, suppress_output
 
 
 def test__freeze_args() -> None:
@@ -39,3 +40,25 @@ def test__freeze_args() -> None:
     with pytest.raises(TypeError) as type_error:
         dummy_function_3({"a": 1})  # type: ignore
     assert "unhashable type: 'dict'" in str(type_error.value)
+
+
+def test__suppress_output(capsys: pytest.CaptureFixture) -> None:
+    """
+    Test 'fm4ar.utils.misc.suppress_output'.
+    """
+
+    # Case 1: Suppress stdout
+    with suppress_output(stdout=True, stderr=False):
+        print("This should not be printed!")
+        print("But this should!", file=sys.stderr)
+    captured = capsys.readouterr()
+    assert captured.out == ""
+    assert captured.err == "But this should!\n"
+
+    # Case 2: Suppress stderr
+    with suppress_output(stdout=False, stderr=True):
+        print("This should be printed!")
+        print("But this should not!", file=sys.stderr)
+    captured = capsys.readouterr()
+    assert captured.out == "This should be printed!\n"
+    assert captured.err == ""
