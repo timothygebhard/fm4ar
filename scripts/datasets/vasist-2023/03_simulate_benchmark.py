f19aa1249b990dec3ebb8c3bd15ef270772c1d3a
Timothy Gebhard <mail@timothygebhard.de>
2024-06-12 18:52:26 +0200

    Update script for simulating default benchmark spectrum

---
+++ scripts/datasets/vasist-2023/03_simulate_benchmark.py
@@ -0,0 +1,107 @@
+"""
+Simulate default benchmark spectrum for the `vasist_2023` dataset.
+"""
+
+import argparse
+import datetime
+import sys
+import time
+from importlib.metadata import version
+
+import h5py
+import numpy as np
+
+from fm4ar.datasets.vasist_2023.prior import THETA_0, SIGMA
+from fm4ar.datasets.vasist_2023.simulator import Simulator
+from fm4ar.utils.environment import get_packages
+from fm4ar.utils.git_utils import get_git_hash
+from fm4ar.utils.paths import get_datasets_dir
+
+
+if __name__ == "__main__":
+
+    script_start = time.time()
+    print("\nSIMULATE VASIST-2023 BENCHMARK SPECTRUM\n", flush=True)
+
+    # Get command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--resolution",
+        type=int,
+        default=400,
+        choices=[400, 1_000],
+        help="Spectral resolution (R = λ/∆λ). Default: 400.",
+    )
+    parser.add_argument(
+        "--time-limit",
+        type=int,
+        default=15,
+        help="Time limit for the simulation in seconds. Default: 15.",
+    )
+    args = parser.parse_args()
+
+    # Set up simulator
+    print("Set up simulator...", end=" ", flush=True)
+    simulator = Simulator(R=args.resolution, time_limit=args.time_limit)
+    print("Done!", flush=True)
+
+    # Simulate benchmark spectrum
+    print("Simulating benchmark spectrum...", end=" ", flush=True)
+    result = simulator(THETA_0.astype(np.float32))
+    if result is None:
+        print("Failed!", flush=True)
+        raise RuntimeError("Simulation failed!")
+    else:
+        wlen, flux = result
+        wlen = wlen.astype(np.float32)
+        flux = flux.astype(np.float32)
+        print("Done!", flush=True)
+
+    # Prepare output directory
+    output_dir = get_datasets_dir() / "vasist-2023" / "benchmark"
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # Gather meta-information to save to HDF file (as attributes)
+    n_bins = len(wlen)
+    prt_version = version("petitRADTRANS")
+    metadata = {
+        "HEAD of fm4ar": get_git_hash(),
+        "Timestamp (UTC)": datetime.datetime.utcnow().isoformat(),
+        "Python version": sys.version,
+        "petitRADTRANS version": version("petitRADTRANS"),
+        "packages": "\n".join(get_packages()),
+    }
+
+    # First, save the noise-free benchmark spectrum
+    print("Saving noise-free benchmark spectrum...", end=" ", flush=True)
+    file_name = f"noise-free__R-{args.resolution}__pRT-{prt_version}.hdf"
+    file_path = output_dir / file_name
+    with h5py.File(file_path, "w") as f:
+        f.attrs.update(metadata)
+        f.create_dataset("wlen", data=wlen.reshape(1, -1))
+        f.create_dataset("flux", data=flux.reshape(1, -1))
+        f.create_dataset("theta", data=THETA_0.reshape(1, -1))
+    print("Done!", flush=True)
+
+    # Next, create noisy versions of the benchmark spectrum
+    # We create 100 noise realizations using the default noise level from
+    # Vasist et al. (2023), i.e., σ = 0.125754 x 10^-16 W / m^2 / µm
+    print("Creating noisy versions...", end=" ", flush=True)
+    rng = np.random.default_rng(seed=42)
+    noise = rng.normal(scale=SIGMA, size=(100, n_bins)).astype(np.float32)
+    noisy_flux = flux + noise
+    print("Done!", flush=True)
+
+    # Save noisy versions of the benchmark spectrum
+    print("Saving noisy versions...", end=" ", flush=True)
+    file_name = f"with-noise__R-{args.resolution}__pRT-{prt_version}.hdf"
+    file_path = output_dir / file_name
+    with h5py.File(file_path, "w") as f:
+        f.attrs.update(metadata | {"noise_level": SIGMA})
+        f.create_dataset("wlen", data=wlen.reshape(1, -1))
+        f.create_dataset("flux", data=noisy_flux)
+        f.create_dataset("noise", data=noise)
+        f.create_dataset("theta", data=np.tile(THETA_0, (100, 1)))
+    print("Done!", flush=True)
+
+    print(f"\nThis took {time.time() - script_start:.1f} seconds!\n")
