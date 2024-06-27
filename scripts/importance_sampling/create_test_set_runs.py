1e74f590b3c24e35f32f1a44e9d780d0d41ff153
Timothy Gebhard <mail@timothygebhard.de>
2024-06-27 03:14:28 +0200

    Update script to handle relative base config paths

--- scripts/importance_sampling/create_test_set_runs.py
+++ scripts/importance_sampling/create_test_set_runs.py
@@ -12,6 +12,7 @@ from subprocess import run
 
 from fm4ar.importance_sampling.config import load_config
 from fm4ar.utils.config import save_config
+from fm4ar.utils.paths import expand_env_variables_in_path
 
 
 def get_cli_arguments() -> argparse.Namespace:
@@ -27,7 +28,9 @@ def get_cli_arguments() -> argparse.Namespace:
         required=True,
         help=(
             "Path to the base configuration file. The run directories "
-            "will be created in the same directory as this file."
+            "will be created in the same directory as this file. This can "
+            "be either an absolute path, or a path relative to the "
+            "`importance_sampling` directory in the experiment directory."
         )
     )
     parser.add_argument(
@@ -74,6 +77,14 @@ if __name__ == "__main__":
     # Get path to launch script
     launch_script = Path(__file__).parent / "run_importance_sampling.py"
 
+    # Resolve the path to the base config in case it is not an absolute path
+    if not args.base_config_path.is_absolute():
+        args.base_config_path = expand_env_variables_in_path(
+            Path(args.experiment_dir)
+            / "importance_sampling"
+            / args.base_config_path
+        )
+
     # Get directory for the runs and load the base configuration
     runs_dir = args.base_config_path.parent
     file_name = args.base_config_path.name
@@ -86,7 +97,9 @@ if __name__ == "__main__":
 
         # Create a new configuration
         config = deepcopy(base_config)
-        config.target_spectrum.file_path = args.target_file_path.as_posix()
+        config.target_spectrum.file_path = expand_env_variables_in_path(
+            args.target_file_path
+        )
         config.target_spectrum.index = idx
 
         # Create the directory for the run
