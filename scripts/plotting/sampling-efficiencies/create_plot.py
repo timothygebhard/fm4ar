f36a0ab5bfd007e78a47646c5066ae17c30c63c5
Timothy Gebhard <mail@timothygebhard.de>
2024-07-23 17:53:32 +0200

    Fix alignment issues; manually define values for `subplots_adjust()`

--- scripts/plotting/sampling-efficiencies/create_plot.py
+++ scripts/plotting/sampling-efficiencies/create_plot.py
@@ -5,6 +5,7 @@ Create scatter plot of sampling efficiency for FMPE / NPE.
 import argparse
 import time
 from pathlib import Path
+from typing import Any
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -49,25 +50,32 @@ if __name__ == "__main__":
     # Setup bins for marginal histograms
     bins = np.linspace(0, 100, 101)
 
+    # Type hints for axes are not supported by matplotlib
+    axes: Any
+
     # Prepare the figure
     print("Creating plot...", end=" ", flush=True)
-    pad_inches = 0.01
     fig, axes = plt.subplots(
         figsize=(
-            config["figsize"][0] / 2.54 - 2 * pad_inches,
-            config["figsize"][1] / 2.54 - 2 * pad_inches,
+            config["figsize"][0] / 2.54,
+            config["figsize"][1] / 2.54,
         ),
         ncols=2,
         nrows=2,
-        width_ratios=(7, 1),
-        height_ratios=(1, 7),
+        gridspec_kw=dict(
+            width_ratios=(7, 1),
+            height_ratios=(1, 7),
+            hspace=0.02,
+            wspace=0.02,
+        ),
+        sharex="col",
+        sharey="row",
     )
 
     # Disable empty axis
     axes[0, 1].axis("off")
 
     # Add scatterplot
-    axes[1, 0].set_box_aspect(1)
     axes[1, 0].set_xlim(-2, 102)
     axes[1, 0].set_ylim(-2, 102)
     axes[1, 0].axline((0, 0), slope=1, color="black", ls="--", lw=0.5)
@@ -77,23 +85,37 @@ if __name__ == "__main__":
         s=config["marker_size"],
         c=config["color"],
     )
-    axes[1, 0].set_xlabel(
-        "FMPE sampling efficiency (in %)",
-        fontsize=config["fontsize_labels"],
-    )
-    axes[1, 0].set_ylabel(
-        "NPE sampling efficiency (in %)",
-        fontsize=config["fontsize_labels"],
-    )
     axes[1, 0].tick_params(
         axis="both",
         length=2,
         labelsize=config["fontsize_ticks"],
     )
 
+    # Add axis labels
+    # We cannot directly use `set_xlabel()` and `set_ylabel()` here because
+    # that will mess with the aspect ratio due to the tick label sizes...
+    axes[1, 0].text(
+        x=50,
+        y=-15,
+        s="FMPE sampling efficiency (in %)",
+        fontsize=config["fontsize_labels"],
+        ha="center",
+        va="top",
+    )
+    axes[1, 0].text(
+        x=-15,
+        y=50,
+        s="NPE sampling efficiency (in %)",
+        fontsize=config["fontsize_labels"],
+        ha="right",
+        va="center",
+        rotation=90,
+    )
+
     # Add marginal histograms at the top
-    axes[0, 0].axis("off")
-    axes[0, 0].set_xlim(-2, 102)
+    axes[0, 0].tick_params(axis="both", length=0)
+    axes[0, 0].set_yticklabels([])
+    axes[0, 0].spines[["left", "right", "top"]].set_visible(False)
     hist_fmpe, *_ = axes[0, 0].hist(
         100 * results["fmpe"]["sampling_efficiency"],
         bins=bins,
@@ -102,8 +124,9 @@ if __name__ == "__main__":
     )
 
     # Add marginal histograms on the right
-    axes[1, 1].axis("off")
-    axes[1, 1].set_ylim(-2, 102)
+    axes[1, 1].tick_params(axis="both", length=0)
+    axes[1, 1].set_xticklabels([])
+    axes[1, 1].spines[["right", "top", "bottom"]].set_visible(False)
     hist_npe, *_ = axes[1, 1].hist(
         100 * results["npe"]["sampling_efficiency"],
         bins=bins,
@@ -128,14 +151,8 @@ if __name__ == "__main__":
 
     # Save the figure
     print("Saving plot...", end=" ", flush=True)
-    plt.subplots_adjust(wspace=0.02, hspace=0.02)
-    fig.tight_layout(pad=0)
-    plt.savefig(
-        Path(__file__).parent / config["output_file_name"],
-        dpi=300,
-        bbox_inches="tight",
-        pad_inches=pad_inches,
-    )
+    fig.subplots_adjust(**config["subplots_adjust"])
+    fig.savefig(Path(__file__).parent / config["output_file_name"])
     print("Done!", flush=True)
 
     print(f"\nThis took {time.time() - script_start:.1f} seconds!\n")
