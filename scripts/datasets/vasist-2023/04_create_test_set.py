5aa57e2a3f3145f6575aedd54e803e2b84e9caee
Timothy Gebhard <mail@timothygebhard.de>
2024-12-03 23:17:20 +0100

    Fix bug

--- scripts/datasets/vasist-2023/04_create_test_set.py
+++ scripts/datasets/vasist-2023/04_create_test_set.py
@@ -222,8 +222,8 @@ if __name__ == "__main__":
         output_dir = args.output_dir
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    # Fix output directory in arguments (otherwise it breaks the HDF export)
-    args.output_dir = str(output_dir)
+    # Remove output dir from metadata (otherwise it breaks the HDF export)
+    del metadata["output_dir"]
 
     # Save target data to HDF file
     print("\nSaving results...", end=" ", flush=True)
